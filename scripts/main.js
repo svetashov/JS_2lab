bf9dce551cc49082d0816d61012aee878a2a9f49
svetashov <grandais1@gmail.com>
2020-06-09 19:15:53 +0300

    Implemented function for hiding all children folders

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,32 @@
 const treeId = 'tree';
 let tree = document.getElementById(treeId);
 
+hideChildrenUL(tree);
+//оборачиваем весь текст в li span'ами
 for (let li of tree.querySelectorAll('li')) {
     let span = document.createElement('span');
     li.prepend(span);
     span.append(span.nextSibling);
 }
 
+// функция, скрывающая все дочерние ul'ы элемента
+function hideChildrenUL(elem) {
+    for (let children of elem.querySelectorAll('ul'))
+        children.hidden = true;
+}
+
 tree.addEventListener('click', (e) => {
-    if (e.target.tagName === 'SPAN');
-    let children = e.target.parentNode.querySelector('ul');
-      if (!children) return;
-      children.hidden = !children.hidden;
+    if (e.target.tagName === 'SPAN') {
+        let children = e.target.parentNode.querySelector('ul');
+            if (children) {
+                let isHiding = !children.hidden;
+                children.hidden = isHiding;
+                // если прячем, то прячем все внутренние.
+                if (isHiding) {
+                    hideChildrenUL(children);
+                }
+                
+            }
+    }
 })
+
